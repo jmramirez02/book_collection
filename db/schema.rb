226e470742e737ff99852cb1b3f9496be4623cef
JamesMRamirez <jmrpersonal02@gmail.com>
2023-09-11 17:04:47 -0500

    Added Author Field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_08_183511) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_11_213842) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_08_183511) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
