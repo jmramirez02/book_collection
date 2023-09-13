46b68e31e2ff84a3d8585b387dc7df7d58db177a
JamesMRamirez <jmrpersonal02@gmail.com>
2023-09-12 23:48:17 -0500

    Book Collection 4 additions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :books
-  root 'books#index'
+  resources :user_books
+  resources :users
+  root 'user_books#index'
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
 end
